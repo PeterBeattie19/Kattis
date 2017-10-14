6a417599b08816aa223b9866a28d5c8eef89d5e8
Peter Beattie <peterbeattie19@gmail.com>
2017-10-14 17:13:08 +0100

    Add files via upload

---
+++ Tarifa.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Tarifa {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int x = scan.nextInt();
+    int size = scan.nextInt();
+
+    //int arr[] =  new int[size];
+    int leftOver = x;
+
+    for(int i = 0; i<size; i++){
+      int usage = scan.nextInt();
+      leftOver -= usage;
+      leftOver += x;
+    }
+
+    System.out.println(leftOver);
+  }
+}
