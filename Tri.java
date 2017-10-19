7b090bba52689ab2e3fa1955a41712f5efe43bc8
Peter Beattie <peterbeattie19@gmail.com>
2017-10-19 13:42:29 +0100

    Add files via upload

---
+++ Tri.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Tri {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int num1 = scan.nextInt();
+    int num2 = scan.nextInt();
+    int num3 = scan.nextInt();
+
+    //Equals sign on the left
+    if(num1==(num2+num3)) System.out.println(num1 +"="+ num2 +"+"+ num3);
+    else if(num1==(num2*num3)) System.out.println(num1 +"="+ num2 +"*"+ num3);
+    else if(num1==(num2/num3)) System.out.println(num1 +"="+ num2 +"/"+ num3);
+    else if(num1==(num2-num3)) System.out.println(num1 +"="+ num2 +"-"+ num3);
+
+    //Equals sign on the right
+    else if((num1+num2)==num3) System.out.println(num1 +"+"+ num2 +"="+ num3);
+    else if((num1*num2)==num3) System.out.println(num1 +"*"+ num2 +"="+ num3);
+    else if((num1/num2)==num3) System.out.println(num1 +"/"+ num2 +"="+ num3);
+    else if((num1-num2)==num3) System.out.println(num1 +"-"+ num2 +"="+ num3);
+
+
+  }
+}
