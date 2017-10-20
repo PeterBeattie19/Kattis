86021212d3880dfa272af28743082b84ea6aa3ac
Peter Beattie <peterbeattie19@gmail.com>
2017-10-20 15:19:30 +0100

    Add files via upload

---
+++ SolvongForCarrots.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class SolvongForCarrots {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int numTest = scan.nextInt();
+    int ans = scan.nextInt();
+    System.out.println(ans);
+
+
+  }
+}
