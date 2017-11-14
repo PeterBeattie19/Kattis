355d61f44c5a78b91c78f7f378d655d4f7dd50f2
Peter Beattie <peterbeattie19@gmail.com>
2017-11-14 19:26:20 +0000

    Add files via upload

---
+++ StarArrangements.py
@@ -0,0 +1,31 @@
+def check (i, j, n):
+  s1 = 0
+  ans = False; 
+  k = 0 
+  while s1 < n:
+    if k%2 == 0:
+      s1 += i
+    else:
+      s1 += j
+    k += 1 
+
+  #print(s1)
+  if s1 == n: ans = True 
+
+  else: ans = False
+  return ans
+
+
+n = int(input())
+print(n, end='')
+print(":") 
+
+for i in range(2,int((n/2)+2)):
+  for j in  range(i-1,i+1):
+    if check(i,j,n) == True :
+      print(i, end='')
+      print(",", end='')
+      print(j) 
+    
+
+
