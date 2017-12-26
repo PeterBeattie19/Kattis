6fc1dc17e1c11e271ecb491612757bfc7d071d50
Peter Beattie <peterbeattie19@gmail.com>
2017-12-26 15:15:54 +0000

    Add files via upload

---
+++ KittenOnATree.java
@@ -0,0 +1,38 @@
+import java.util.*; 
+import java.io.*; 
+
+public class KittenOnATree {
+	public static void main (String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+		String start = br.readLine(); 
+		String line[] = br.readLine().split(" "); 
+		ArrayList<String[]> graph = new ArrayList<>(); 
+		while(Integer.parseInt(line[0]) != -1){
+			//int parent = Integer.parent(line[0]); 
+
+			graph.add(line); 
+			line = br.readLine().split(" "); 
+		}
+
+		//Searching for start 
+
+		do {
+			System.out.print(start +" "); 
+			start = findParent(start, graph); 
+		}
+
+		while(start.matches("-1") == false); 
+	}
+
+	public static String findParent(String start, ArrayList<String[]> graph ){
+		for(String s[]: graph){
+			for(int i = 1; i<s.length; i++){
+				if(s[i].matches(start)){
+					return s[0]; 
+				}
+			}
+		}
+		return "-1"; 
+	}
+} 
