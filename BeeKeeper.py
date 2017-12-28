8aa604302807d0354f1144540d14aba71098de2e
Peter Beattie <peterbeattie19@gmail.com>
2017-12-28 00:04:57 +0000

    Update BeeKeeper.py

--- BeeKeeper.py
+++ BeeKeeper.py
@@ -2,7 +2,7 @@ def countDoubles(s1):
     c = 0
     arr = ['a','o','u','i','e','y'] 
     for i in range(0,len(s1)-1):
-        if s1[i] in arr and s1[i] == s1[i+1] and s1[i] != s1[i-1]:
+        if s1[i] in arr and s1[i] == s1[i+1]:
             c += 1
     return c
 
@@ -11,7 +11,8 @@ n = int(input())
 while n != 0:
     arr = []
     ans = ''
-    m = 0
+    m = -1
+    count = 0
     for _ in range(n):
         s = str(input())
         count = countDoubles(s)
