2ec861fab8890bce12df72d6ed0316155534c054
Peter Beattie <peterbeattie19@gmail.com>
2018-03-13 21:17:52 +0000

    Delete CobinationLock.py

--- CobinationLock.py
+++
@@ -1,6 +0,0 @@
-while True:
-    arr = list(map(int, input().split()))
-
-    if sum(arr) == 0:
-        break
-    print((120 + (arr[0] - arr[1] + 40) % 40 + (arr[2] - arr[1] + 40) % 40 + (arr[2] - arr[3] + 40) %40)*9)
