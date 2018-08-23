198be136c93c7e4a678ba2af84536c3ab8230465
Peter Beattie <peterbeattie19@gmail.com>
2018-08-23 19:42:26 +0100

    upload

---
+++ Push.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+typedef long long ll;
+
+#define endl "\n"
+#define rep(i,a,b) for(int i = a; i<b; i++)
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+
+	return 0; 
+}
