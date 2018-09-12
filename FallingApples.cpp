682f606813264d71905b24114f81cceeecb754c6
Peter Beattie <peterbeattie19@gmail.com>
2018-09-12 10:43:42 +0100

    none

---
+++ FallingApples.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl "\n"
+#define rep(i,a,b) for(int i = a; i<b; i++)
+
+    int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int i,j,k;
+    
+    int r, c;
+    scanf("%ld %ld", &r, &c); 
+
+    vector<char*[]> grid; 
+
+    rep(i, 0, r){
+        char* buffer[c]; 
+        grid.push_back(buffer) 
+    }
+
+
+    
+    return 0;
+}
