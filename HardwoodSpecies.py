7aa7e128cd0cb54297ddcd05778a190bc427d899
Peter Beattie <peterbeattie19@gmail.com>
2018-10-26 20:29:41 +0100

    done

--- HardwoodSpecies.py
+++ HardwoodSpecies.py
@@ -1,27 +1,30 @@
 import sys
 import heapq
 
-mp = {}
-s = set()
-a = []
-c = 0
-for line in sys.stdin:
-    if line in mp:
-        mp[line] += 1
-    else:
-        mp[line] = 1
+def main():
+    mp = {}
+    s = set()
+    a = []
+    c = 0
+    for line in sys.stdin:
+        if line in mp:
+            mp[line] += 1
+        else:
+            mp[line] = 1
 
-    if line not in s:
-        s.add(line)
-        heapq.heappush(a, line) 
-    c += 1
+        if line not in s:
+            s.add(line)
+            heapq.heappush(a, line) 
+        c += 1
+        
+        #if line == '0': break
+
+    size = len(a)
+    while len(a) != 0:
+        i = heapq.heappop(a) 
+        sys.stdout.write(i +" "+ str(float((mp[i]/c)*100)) +"\n")
+    sys.stdout.flush() 
+
     
-    #if line == '0': break
-
-size = len(a)
-while len(a) != 0:
-    i = heapq.heappop(a) 
-    sys.stdout.write(i +" "+ str(float((mp[i]/c)*100)) +"\n")
-sys.stdout.flush() 
-
-    
+if __name__ == "__main__":
+    main() 
