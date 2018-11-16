8ccb42ab8019763628d0db31d28b9abcad0202ae
Peter Beattie <peterbeattie19@gmail.com>
2018-11-16 19:11:22 +0000

    NOT WORKING

---
+++ Brexit.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl "\n"
+#define rep(i,a,b) for(int i = a; i<b; i++)
+
+inline void solve(vector<set<int>>& graph, int cur, set<int>& left, vector<int>& original_sizes){
+    if(left.count(cur) == 0 || graph[cur].size() <= original_sizes[cur]/2 || graph[cur].size() == 1){
+        left.insert(cur); 
+        for(auto &  i: graph[cur]){
+            if(graph[i].count(cur) != 0)
+                graph[i].erase(cur);
+        }
+
+        for(auto &i : graph[cur]){
+            if(graph[i].size() <= original_sizes[i]/2 || graph[i].size() == 1)
+                solve(graph, i, left, original_sizes); 
+        }
+    
+}
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int i,j,k,a,b,c,p,x,L;
+    
+    cin >> c >> p >> x >> L; 
+
+    x -= 1; L -= 1; 
+
+    vector<set<int>> graph(c); 
+
+    for(int e = 0; e<p; e++){
+        cin >> a >> b;
+        a -= 1; b -= 1;
+        graph[a].insert(b); 
+        graph[b].insert(a); 
+    }
+
+    vector<int> original_sizes; 
+
+    rep(i, 0, c){
+        original_sizes.push_back(graph[i].size()); 
+    }
+
+    set<int> left; 
+
+    solve(graph, L, left, original_sizes);
+
+    if(left.count(x) != 0)
+        cout << "leave" << endl; 
+    else
+        cout << "stay" << endl; 
+
+    return 0;
+}
--- Brexit.py
+++ Brexit.py
@@ -1,21 +1,22 @@
-from math import ceil
+import sys
 def solve(graph, cur, left, original_sizes):
-    if cur in left or len(graph[cur]) < ceil(original_sizes[cur]/2): 
+    if cur not in left or len(graph[cur]) <= (original_sizes[cur]/2) or (len(graph[cur]) == 1): 
         left.add(cur) 
         for i in graph[cur]:
-            graph[i].remove(cur) 
+            if cur in graph[i]:
+                graph[i].remove(cur) 
         for i in graph[cur]:
-            if  len(graph[i]) <= ceil(original_sizes[i]/2):
-                print(cur) 
+            if  len(graph[i]) <= (original_sizes[i]/2) or (len(graph[i]) == 1 and original_sizes[i] > 1):
+                #print(cur)
                 solve(graph, i, left, original_sizes) 
 
-c, p, x, L = map(int, input().split()) 
+c, p, x, L = map(int, sys.stdin.readline().split()) 
 x -= 1 ; L -= 1
 
 graph = [set() for _ in range(c)]
 
 for _ in range(p):
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     a -= 1 ; b -= 1
     graph[a].add(b)
     graph[b].add(a) 
@@ -23,10 +24,14 @@ for _ in range(p):
 original_sizes = list(map(len, graph)) 
 
 left = set()
-left.add(L) 
-solve(graph, L, left, original_sizes) 
+#left.add(L) 
+try:
+    solve(graph, L, left, original_sizes) 
+except Exception:
+    print("leave")
+    exit()
 
-if x in left or len(graph[x]) < ceil(original_sizes[x]/2):
+if x in left:
     print("leave")
 else:
     print("stay")
